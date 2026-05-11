c1376809577172b30eaad6c5f2e4b1f8cb965e2f
hyeonLewis <lewis.kim@kaia.io>
2026-05-12 07:45:40 +0900

    work: bounded tx selection during block building

--- cmd/utils/flaggroup.go
+++ cmd/utils/flaggroup.go
@@ -54,6 +54,7 @@ var FlagGroups = []FlagGroup{
 			StartBlockNumberFlag,
 			BlockGenerationIntervalFlag,
 			BlockGenerationTimeLimitFlag,
+			BlockGenerationTxLimitFlag,
 			OpcodeComputationCostLimitFlag,
 			UseConsoleLogFlag,
 		},
--- work/worker.go
+++ work/worker.go
@@ -439,8 +439,7 @@ func (self *worker) commitNewWork(trigger newWorkTrigger) {
 		header.ExcessBlobGas = &excessBlobGas
 	}
 	// Could potentially happen if starting to mine in an odd state.
-	err = self.makeCurrent(parent, header)
-	if err != nil {
+	if err := self.makeCurrent(parent, header); err != nil {
 		logger.Error("Failed to create mining context", "err", err)
 		return
 	}
@@ -721,7 +720,8 @@ func (env *Task) ApplyTransactions(txs *types.TransactionsByPriceAndNonce, bc Bl
 		RunningEVM: chEVM,
 	}
 
-	arrayTxs := builder.Arrayify(txs)
+	txLimit := params.BlockGenerationTxLimit
+	arrayTxs := builder.ArrayifyByCount(txs, txLimit)
 	incorporatedTxs, bundles := builder.ExtractBundlesAndIncorporate(arrayTxs, txBundlingModules)
 	totalBundles := len(bundles)
 
@@ -763,6 +763,9 @@ CommitTransactionLoop:
 				builder.PopTxs(&incorporatedTxs, numShift, &bundles, env.signer)
 				continue
 			}
+		}
+		if txLimit > 0 && numTxsChecked+int64(numShift) > int64(txLimit) {
+			break
 		}
 
 		tx, err := txOrGen.GetTx(env.state.GetNonce(nodeAddr))
@@ -901,7 +904,6 @@ CommitTransactionLoop:
 			)
 		}
 	}
-
 	// Update the number of transactions checked and dropped during ApplyTransactions.
 	checkedTxsGauge.Update(numTxsChecked)
 	nonceTooLowTxsGauge.Update(numTxsNonceTooLow)
